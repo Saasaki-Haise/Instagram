5e4912e2f9f995648be2680aca220b921e128678
Haise <yanis.badalati@gmail.com>
2020-01-28 14:08:07 +0100

    Initial Commit

---
+++ Instagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagram
+//
+//  Created by Haise on 28/01/2020.
+//  Copyright © 2020 Haise. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
